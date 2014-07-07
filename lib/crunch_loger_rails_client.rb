32fb4723c812a7f9fd5649f88f8ccd25be927bd1
Dawid <d.ratka@exlabs.pl>
2014-07-07 23:19:04 +0200

    logger

--- lib/crunch_loger_rails_client.rb
+++ lib/crunch_loger_rails_client.rb
@@ -1,2 +1,30 @@
+require 'net/http'
 module CrunchLogerRailsClient
+	class LogSubscriber < ActiveSupport::LogSubscriber
+    def process_action event
+      payload = event.payload
+      param_method = payload[:params]['_method']
+      method = param_method ? param_method.upcase : payload[:method]
+      status = compute_status(payload)
+      path = payload[:path]
+      params = payload[:params]#.except(*INTERNAL_PARAMS)
+
+      message = "%-6s #{status} #{path}" % method
+      message << " parameters=#{params}" unless params.empty?
+      uri = URI.parse("http://localhost:3001/logger_test")
+      Net::HTTP.post_form(uri, {:api_key => "asdqwetgzvasd", :payload => payload})
+      # binding.pry
+    end
+
+    def compute_status payload
+      status = payload[:status]
+      if status.nil? && payload[:exception].present?
+        exception_class_name = payload[:exception].first
+        status = ActionDispatch::ExceptionWrapper.status_code_for_exception(exception_class_name)
+      end
+      status
+    end
+  end
 end
+
+CrunchLogerRailsClient::LogSubscriber.attach_to :action_controller
